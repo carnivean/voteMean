02e4d38bbf9fa15f56eabee77e7d1c3970476733
Erik Kynast <erikkynast@gmail.com>
2015-09-24 15:20:59 +0200

    poll i shown, ugly as fuck
    Refactoring:
    header outsourced

--- client/app/vote/vote.controller.js
+++ client/app/vote/vote.controller.js
@@ -2,7 +2,25 @@
  * Created by Erik Kynast on 24.09.2015.
  */
 angular.module('meanVoteApp')
-  .controller('VoteCtrl', function ($scope, $http, Auth, $route, $location, $routeParams) {
+  .controller('VoteCtrl', function ($scope, $http, Auth, $route, $location, $routeParams, $log) {
+    console.log($routeParams);
     $scope.$routeParams = $routeParams;
     $scope.$location = $location;
+
+    $scope.pollFetched = false;
+
+    var submitPoll = function() {
+      var apiString = '/api/polls/' + $scope.$routeParams.username + '/' + $scope.$routeParams.question;
+      $log.log('apiString: ' + apiString);
+
+      $http.get(apiString).success(function(data) {
+          $scope.$pollData = data;
+          console.log('successful:');
+          console.log(data);
+      }).error(function(data) {
+        console.log('Error: ' + data);
+      });
+    };
+
+    submitPoll();
   });
