5bb1245a8791353df3207e8fa83a4ca1f68e3b56
Erik Kynast <erikkynast@gmail.com>
2015-09-24 09:15:41 +0200

    Dashboard - ng-include newPoll works

---
+++ client/components/newpoll/newpoll.controller.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Erik Kynast on 24.09.2015.
+ */
+
+'use strict';
+
+angular.module('meanVoteApp')
+  .controller('NewPollCtrl', function ($scope, $location, Auth) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
