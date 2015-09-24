8198b89abb231919fce6e7d66fadea67d2b9421c
Erik Kynast <erikkynast@gmail.com>
2015-09-24 08:58:29 +0200

    Working isLoggedIn() Checking for Welcomes Login

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('meanVoteApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location) {
+  .controller('LoginCtrl', function ($scope, Auth, $location, $window) {
     $scope.user = {};
     $scope.errors = {};
 
@@ -23,4 +23,7 @@ angular.module('meanVoteApp')
       }
     };
 
+    $scope.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
   });
